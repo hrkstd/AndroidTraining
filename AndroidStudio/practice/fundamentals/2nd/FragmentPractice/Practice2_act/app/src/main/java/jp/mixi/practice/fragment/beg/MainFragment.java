a66d1b58e818bc797619dcc207402f1e442becc7
hhonjo <h.honjo.lvt@knowledge-case.co.jp>
2015-11-24 12:58:20 +0900

    add practice_2nd_Fragment_02

---
+++ AndroidStudio/practice/fundamentals/2nd/FragmentPractice/Practice2_act/app/src/main/java/jp/mixi/practice/fragment/beg/MainFragment.java
@@ -0,0 +1,40 @@
+package jp.mixi.practice.fragment.beg;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainFragment extends Fragment {
+
+    public static MainFragment newInstance(String param1, String param2) {
+        MainFragment fragment = new MainFragment();
+        return fragment;
+    }
+
+    public MainFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_01, container, false);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
