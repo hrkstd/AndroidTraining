3bf133f7f28995ca960845879a8f29ac460a6524
hhonjo <h.honjo.lvt@knowledge-case.co.jp>
2015-11-24 12:59:32 +0900

    add assignments_2nd_ControllerAssignment_01

---
+++ AndroidStudio/assignments/fundamentals/2nd/ControllerAssignment1_act/app/src/main/java/jp/mixi/assignment/controller/beg/MainActivity.java
@@ -0,0 +1,57 @@
+
+package jp.mixi.assignment.controller.beg;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Toast;
+
+/**
+ * TODO: 課題1
+ * {@link Toast#makeText(Context, CharSequence, int)} または
+ * {@link Toast#makeText(Context, int, int)} を利用して、各ライフサイクルメソッドがどのような順番で
+ * 実行されているかを確認してください。
+ * 確認したら、assignments/fundamentals/2nd/Report.md にその順番を記述してください。
+ * @author keishin.yokomaku
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
